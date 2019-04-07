190442667071ed990bfc798939bc68a8d78ce50f
AmChaoSun <leosunchao@gmail.com>
2019-04-08 00:41:58 +1000

    enclose try catch

--- C2CApp/Program.cs
+++ C2CApp/Program.cs
@@ -7,28 +7,37 @@ namespace C2CApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C2C!");
-
-            bool running = true;
-            var app = CommandCtrlApp.Instance;
-            while (running)
+            try
             {
-                app.ShowMenu();
-                string operation = Console.ReadLine();
-                switch (operation)
+                Console.WriteLine("Welcome to C2C!");
+
+                bool running = true;
+                var app = CommandCtrlApp.Instance;
+                while (running)
                 {
-                    case "select device": app.SelectDevice(); break;
-                    case "add device": app.AddDevice(); break;
-                    case "remove device": app.RemoveDevice(); break;
-                    case "exit":
-                        running = false;
-                        break;
-                    default:
-                        Console.WriteLine("invalid operation");
-                        Console.WriteLine();
-                        break;
+                    app.ShowMenu();
+                    string operation = Console.ReadLine();
+                    switch (operation)
+                    {
+                        case "select device": app.SelectDevice(); break;
+                        case "add device": app.AddDevice(); break;
+                        case "remove device": app.RemoveDevice(); break;
+                        case "exit":
+                            running = false;
+                            break;
+                        default:
+                            Console.WriteLine("invalid operation");
+                            Console.WriteLine();
+                            break;
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine("please contact Charles on leosunchao@gmail.com.");
+            }
+
         }
     }
 }
